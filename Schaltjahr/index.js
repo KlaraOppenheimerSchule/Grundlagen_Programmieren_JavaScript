bdc56d8b246ad3b96d4e836a5a6802e953ad6b4a
chrac <christian.goch@googlemail.com>
2021-11-18 09:18:14 +0100

    polishing

--- Schaltjahr/index.js
+++ Schaltjahr/index.js
@@ -4,15 +4,17 @@
             year = prompt("What year do you want to check ?");
             var result;
             if (year == 0 || year == "") {
-                alert("Geben sie ein richtiges Jahr ein!");
+                alert("You have to give a real year!");
             };
             if (year % 4 == 0 && year % 100 != 0 || (year % 400 == 0)) {
                 result = true;
                 console.log(result);
+                alert(year + " is a leap Year = " + result);
                 return result;
             } else {
                 result = false;
                 console.log(result);
+                alert(year + " is a leap Year = " + result);
                 return result;
             };
             
